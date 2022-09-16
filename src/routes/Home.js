b98a794c106f61cdac5cb5b91570728db2e34332
hasibulislam999 <hasib143sl@gmail.com>
2022-09-16 18:32:21 +0600

    home route integrated

---
+++ src/routes/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Filter from "../components/filter/Filter";
+import Table from "../components/table/Table";
+
+const Home = () => {
+  return (
+    <section>
+      <Filter />
+      <Table />
+    </section>
+  );
+};
+export default Home;
