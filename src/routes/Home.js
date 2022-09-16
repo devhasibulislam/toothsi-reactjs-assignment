7231ff9e7a77cc6ae3293044525b3ae86ef9b1f4
hasibulislam999 <hasib143sl@gmail.com>
2022-09-16 22:53:03 +0600

    home modified with child-parent props

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,12 +1,16 @@
 import React from "react";
+import { useState } from "react";
 import Filter from "../components/filter/Filter";
 import Table from "../components/table/Table";
 
 const Home = () => {
+  const [keyword, setKeyword] = useState("");
+  const [filter, setFilter] = useState([]);
+
   return (
     <section>
-      <Filter />
-      <Table />
+      <Filter setKeyword={setKeyword} setFilter={setFilter} />
+      <Table keyword={keyword} filter={filter} />
     </section>
   );
 };
