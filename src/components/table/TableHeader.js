b09faae1aa114a57bb9ed50ea58289d723203bcd
hasibulislam999 <hasib143sl@gmail.com>
2022-09-16 18:31:58 +0600

    table header refactored

---
+++ src/components/table/TableHeader.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const TableHeader = () => {
+  return (
+    <tr>
+      <th>ID</th>
+      <th>Image</th>
+      <th>Name</th>
+      <th>Color</th>
+      <th>Stock</th>
+      <th>Price</th>
+      <th>Buy</th>
+    </tr>
+  );
+};
+export default TableHeader;
