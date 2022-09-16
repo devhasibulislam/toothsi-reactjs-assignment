a80558f04923cc3b614e8bffa8830e5d104ea9fd
hasibulislam999 <hasib143sl@gmail.com>
2022-09-16 18:32:14 +0600

    greeting routes integrated

---
+++ src/routes/Greetings.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Greetings = () => {
+  return (
+    <section className="flex justify-center items-center h-screen w-screen">
+        <img 
+        src="http://montco.happeningmag.com/wp-content/uploads/2014/11/thankyou.png" 
+        alt="TYSM" 
+        className="max-w-full"
+        />
+    </section>
+  );
+};
+export default Greetings;
