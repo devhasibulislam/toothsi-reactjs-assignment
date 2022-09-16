d2f05c7f6aec58aaf315186a0576afc5debc6703
hasibulislam999 <hasib143sl@gmail.com>
2022-09-16 18:31:41 +0600

    checkout row content adding done

---
+++ src/components/checkout/CheckoutRow.js
@@ -0,0 +1,38 @@
+import React from "react";
+import useProduct from "../../utilities/useProduct";
+
+const CheckoutRow = () => {
+  const products = useProduct();
+  return products.slice(0, 5).map((product) => (
+    <tr key={product._id} className="hover">
+      <th>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-6 h-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M6 18L18 6M6 6l12 12"
+          />
+        </svg>
+      </th>
+      <th>
+        <img
+          src={product.thumbnail}
+          alt={product.name}
+          className="max-w-full w-12 shadow rounded-md p-1 object-cover"
+        />
+      </th>
+      <td>{product.name}</td>
+      <td>{product.price}</td>
+      <td>QTY</td>
+      <td>Subtotal</td>
+    </tr>
+  ));
+};
+export default CheckoutRow;
