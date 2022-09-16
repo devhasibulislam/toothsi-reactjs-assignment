0faf5e97a1a0a5fd9ce1972e2fe51d09d1516a95
hasibulislam999 <hasib143sl@gmail.com>
2022-09-16 22:51:50 +0600

    filter modified with keyword and category and sizes

--- src/components/filter/Filter.js
+++ src/components/filter/Filter.js
@@ -1,16 +1,17 @@
 import React from "react";
+// import { useState } from "react";
 import LeftFilter from "./LeftFilter";
 import RightFilter from "./RightFilter";
 
-const Filter = () => {
+const Filter = ({setKeyword, setFilter}) => {
   return (
     <section className="shadow-lg py-4 lg:px-0 px-4">
       <div className="flex lg:flex-row md:flex-row flex-col lg:gap-y-0 md:gap-y-0 gap-y-4 justify-between items-center container mx-auto">
         {/* header left */}
-        <LeftFilter />
+        <LeftFilter setFilter={setFilter} />
 
         {/* header right */}
-        <RightFilter />
+        <RightFilter setKeyword={setKeyword} />
       </div>
     </section>
   );
