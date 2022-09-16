5b738701ade878f72d194d4f84c3cc0b2cce0471
hasibulislam999 <hasib143sl@gmail.com>
2022-09-16 23:10:17 +0600

    implement reset button on left filter content

--- src/components/filter/LeftFilter.js
+++ src/components/filter/LeftFilter.js
@@ -3,8 +3,8 @@ import { useEffect } from "react";
 import { useState } from "react";
 
 const LeftFilter = ({ setFilter }) => {
-  const [category, setCategory] = useState("");
-  const [size, setSize] = useState("");
+  const [category, setCategory] = useState("Categories");
+  const [size, setSize] = useState("Size");
 
   useEffect(() => {
     setFilter([category, size]);
@@ -18,9 +18,11 @@ const LeftFilter = ({ setFilter }) => {
           <select
             className="select select-bordered select-sm w-full max-w-xs rounded-sm"
             onChange={(e) => setCategory(e.target.value)}
+            defaultValue={category}
+            value={category}
           >
-            <option disabled selected>
-              Categories
+            <option value={category} disabled selected>
+              {category}
             </option>
             <option value={"apparel"}>Apparel</option>
             <option value={"footwear"}>Footwear</option>
@@ -36,9 +38,11 @@ const LeftFilter = ({ setFilter }) => {
           <select
             className="select select-bordered select-sm w-full max-w-xs rounded-sm"
             onChange={(e) => setSize(e.target.value)}
+            defaultValue={size}
+            value={size}
           >
-            <option disabled selected>
-              Sizes
+            <option value={size} disabled selected>
+              {size}
             </option>
             <option value={"XL"}>XL</option>
             <option value={"L"}>L</option>
@@ -50,23 +54,23 @@ const LeftFilter = ({ setFilter }) => {
         {/* reset button */}
         <button
           className="btn btn-sm text-[#009ebe] rounded-sm flex gap-x-2 bg-white"
-          // onClick={() => {
-          //   setCategory("Categories");
-          //   setSize("Size");
-          // }}
+          onClick={() => {
+            setCategory("Categories");
+            setSize("Size");
+          }}
         >
           <span>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
-              stroke-width="1.5"
+              strokeWidth="1.5"
               stroke="currentColor"
               className="w-6 h-6"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLinecap="round"
+                strokeLinejoin="round"
                 d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
               />
             </svg>
