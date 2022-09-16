13d7e2f968abe8787ed32bded61e18d17aa76065
hasibulislam999 <hasib143sl@gmail.com>
2022-09-16 15:48:38 +0600

    driver modification done

---
+++ src/App.js
@@ -0,0 +1,18 @@
+/**
+ * Title: Product listing page
+ * Description: The idea is to create a Product Listing page and cart (checkout) summary page of an ecommerce website which has certain functionalities.
+ * Author: Hasibul Islam
+ * Date: 16/09/2022
+ */
+
+import "./App.css";
+import Filter from "./components/filter/Filter";
+
+function App() {
+  return (
+    <section className="App">
+      <Filter />
+    </section>
+  );
+}
+export default App;
