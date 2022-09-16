13b8e35a8c7d47a3710ba7d09635c856df3b19b8
hasibulislam999 <hasib143sl@gmail.com>
2022-09-16 23:24:00 +0600

    icon tooltip added

--- src/components/checkout/CheckoutRow.js
+++ src/components/checkout/CheckoutRow.js
@@ -6,20 +6,22 @@ const CheckoutRow = () => {
   return products.slice(0, 5).map((product) => (
     <tr key={product._id} className="hover">
       <th>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="currentColor"
-          className="w-6 h-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M6 18L18 6M6 6l12 12"
-          />
-        </svg>
+        <span className="tooltip tooltip-right" data-tip="Remove from Cart" role={"button"}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        </span>
       </th>
       <th>
         <img
