3ac60aa1559c3c205807c0a51605fba88eaa1496
hasibulislam999 <hasib143sl@gmail.com>
2022-09-18 23:11:43 +0600

    modification on filter

--- src/components/filter/Filter.js
+++ src/components/filter/Filter.js
@@ -3,7 +3,7 @@ import React from "react";
 import LeftFilter from "./LeftFilter";
 import RightFilter from "./RightFilter";
 
-const Filter = ({setKeyword, setFilter}) => {
+const Filter = ({ setKeyword, setFilter, selectMultipleProducts }) => {
   return (
     <section className="shadow-lg py-4 lg:px-0 px-4">
       <div className="flex lg:flex-row md:flex-row flex-col lg:gap-y-0 md:gap-y-0 gap-y-4 justify-between items-center container mx-auto">
@@ -11,7 +11,10 @@ const Filter = ({setKeyword, setFilter}) => {
         <LeftFilter setFilter={setFilter} />
 
         {/* header right */}
-        <RightFilter setKeyword={setKeyword} />
+        <RightFilter
+          setKeyword={setKeyword}
+          selectMultipleProducts={selectMultipleProducts}
+        />
       </div>
     </section>
   );
